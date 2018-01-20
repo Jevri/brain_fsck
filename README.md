a7b241348568f0d613f39102cd6f08eb112685e7
jevri <viktoregebjerg@gmail.com>
2018-01-20 16:15:59 +0100

    added bf+ commands and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # brain_fsck
 a brain fuck interpretor
+
+## Spesifications
+..* tape diamentions: 30000 cells 512 are in the negative dirrection
+..* cell: cells are signed intengers where the size are system dependent, on m..* ost systems it is 32 bit
+..* cell wrap: cell wrap is suported
+..* EOF: EOF is system dependent
+
+## bf+
+this version of brain fuck has implimented bf+ commands
+#bf+ commands
+..* break point: `#` will write out a part of the current memory
+..* decimal diget: will be asigned to current memory cell
+..* print as decimal: `:` will print current call in decimal
--- brain_fuck.c
+++ brain_fuck.c
@@ -3,59 +3,69 @@
 #include <string.h>
 
 int
-ae_load_file_to_memory(const char *filename, char **result) 
+fsize(FILE *fp)
 {
   int size = 0;
-  FILE *f = fopen(filename, "r");
-  if (f == NULL) 
-    { 
-      *result = NULL;
-      return -1; // -1 means file opening fail 
-    } 
-  fseek(f, 0, SEEK_END);
-  size = ftell(f);
-  fseek(f, 0, SEEK_SET);
-  *result = (char *)malloc(size+1);
-  if (size != fread(*result, sizeof(char), size, f)) 
-    { 
-      free(*result);
-      return -2; // -2 means file reading fail 
-    } 
-  fclose(f);
-  (*result)[size] = 0;
+  fseek(fp, 0, SEEK_END);
+  size = ftell(fp);
+  fseek(fp, 0, SEEK_SET);
   return size;
 }
 
-int
-main(int argc,char *argv[])
+int //return size of result
+load_file_to_memory(const char *filename, char **result) 
 {
-  int memory[30000] = {0};
-  int mem_point = 512;
-  int loop_stack[256];
-  int loop_top = 0;
+  FILE *fp = fopen(filename, "r");
+  if (f == NULL) 
+    { 
+      *result = NULL;
+      return -1;
+    } 
+  int size = fsize(fp);
+  *result = malloc(size + 1);
+  if (size != fread(*result, sizeof(char), size, fp)) 
+    { 
+      free(*result);
+      return -2;
+    } 
+  fclose(f);
+  (*result)[size] = 0;
+  return size + 1;
+}
+
+int bf(int,char*);
+
+int
+main(int argc, char *argv[])
+{
   char * file_path = (argc >= 2) ? argv[1] : "script.bf";
   char * str;
-  int end = 0;
-  //printf("%s\n",file_path);
-  int str_length = ae_load_file_to_memory(file_path,&str);
+  int str_length = load_file_to_memory(file_path, &str);
   if (str_length < 0)
     {
       puts("error loading file");
       return 1;
     }
-  //printf("%d\n",str_length);
+  bf(str_length, str);
+  return 0;
+}
+
+int get_op(char *script, int script_length, int *index, char * op);
+
+int
+bf(int script_length, char * script)
+{
+  int memory[30000] = {0};
+  int mem_point = 512;
+  int loop_stack[256];
+  int loop_top = 0;
+  int skip_loop_end = 0;
   int i;
-  for (i = 0; i < str_length; i++)
+  char op[100];
+  for (i = 0; i < script_length -1; i++)
     {
-/*
-      printf("%d %c %d %d %d\n",
-	     i,
-	     str[i],
-	     loop_top,
-	     mem_point - 512,
-	     memory[mem_point]);
-*/
-      switch (str[i])
+      
+      switch (get_op(script, script_length, &i, op))
 	{
 	case '+':
 	  memory[mem_point]++;
@@ -73,24 +83,73 @@ main(int argc,char *argv[])
           if (memory[mem_point])
 	    loop_stack[loop_top++] = i - 1;
           else
-	    while (str[++i] != ']' || end)
-	      if (str[i] == '[')
-		end++;
+	    while (script[++i] != ']' || skip_loop_end)
+	      if (script[i] == '[')
+		skip_loop_end++;
               else if (str[i] == ']')
-		end--;
+		skip_loop_end--;
           break;
 	case ']':
           i = loop_stack[--loop_top];
 	  break;
 	case '.':
 	  putchar(memory[mem_point]);
-//putchar('-');
 	  break;
 	case ',':
 	  memory[mem_point] = getchar();
 	  break;
+	case ':':
+	  printf("%d\n",memory[mem_point]);
+	  break;
+	case '#':
+	  int to;
+	  int from;
+	  if (30000 - mem_point < 5)
+	    {
+	      to = 2999;
+	      from = to - 10;
+	    }
+	  else if (mem_point < 5)
+	    {
+	      from = 0;
+	      to = from + 10;
+	    }
+	  else
+	    {
+	      to = mem_point + 5;
+	      from = mem_point - 5;
+	    }
+	  int j;
+	  for (j = from; j < to; j++)
+	    if (j == mem_point)
+	      printf("\n%d<\n", memory[j]);
+	    else
+	      printf("\n%d\n", memory[j]);
+	  break;
+	case '0':
+	  memory[mem_point] = atoi(op);
+	  break;
+	  
 	}
     }
-  putchar('\n');
   return 0;
 }
+
+#include <ctype.h>
+
+int
+get_op(char *script,int script_length, int *index, char * op)
+{
+  if (!isdigit(script[*index]))
+    return script[*index];
+  int i;
+  for (i = 0; isdigit(script[*index]); i++)
+    {
+      op[i] = script[*index];
+      *index++;
+    }
+  *index--;
+  op[i] = 0;
+  return '0';
+}
+
